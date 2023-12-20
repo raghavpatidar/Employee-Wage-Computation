173b23f9c630f81b33917f719d9e2fe4923c49e5
raghavpatidar <raghavpatidar997764@gmail.com>
2023-12-20 17:54:54 +0530

    MonthlyWage

--- src/App.java
+++ src/App.java
@@ -3,27 +3,36 @@ public class App {
     public static final int WAGE_PER_HOUR = 20;
     public static final int FULL_TIME = 8;
     public static final int PART_TIME = 4;
+    public static final int WORKING_DAY_IN_MONTH = 20;
 
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome To Employee Wage Computation Program");
 
-        int employeeType = isPresent();
+        int totalMonthWage = 0;
+        int workingDay = 0;
+        while (workingDay < WORKING_DAY_IN_MONTH) {
+            int employeeType = isPresent();
+            switch (employeeType) {
+                case 1:
+                    System.out.println("Employee is FullTime DailyWage: " + calculateWage(FULL_TIME));
+                    totalMonthWage += calculateWage(FULL_TIME);
+                    workingDay++;
+                    break;
+                case 2:
+                    System.out.println("Employee is PartTime DailyWage: " + calculateWage(PART_TIME));
+                    totalMonthWage += calculateWage(PART_TIME);
+                    workingDay++;
+                    break;
+                case 3:
+                    System.out.println("Employe is Absent DailyWage: " + calculateWage(0));
+                    break;
 
-        switch (employeeType) {
-            case 1:
-                System.out.println("Employee is FULL Time and it's Daily Wage is : " + calculateWage(FULL_TIME));
-                break;
-            case 2:
-                System.out.println("Employee is PART Time and it's Daily Wage is : " + calculateWage(PART_TIME));
-                break;
-            case 3:
-                System.out.println("Employe is Absent  and it's Daily Wage is : " + calculateWage(0));
-                break;
-
-            default:
-                System.out.println("This Person Is not An Employee");
-                break;
+                default:
+                    System.out.println("This Person Is not An Employee");
+                    break;
+            }
         }
+        System.out.println("Total Wage For Month : " + totalMonthWage);
     }
 
     private static int calculateWage(int time) {
