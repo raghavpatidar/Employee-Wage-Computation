4c6d04fbffd132dd6963d4cae18478df41f4680d
raghavpatidar <raghavpatidar997764@gmail.com>
2023-12-20 18:00:11 +0530

    MonthHour

--- src/App.java
+++ src/App.java
@@ -4,23 +4,27 @@ public class App {
     public static final int FULL_TIME = 8;
     public static final int PART_TIME = 4;
     public static final int WORKING_DAY_IN_MONTH = 20;
+    public static final int TOTAL_WORKING_HOUR = 100;
 
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome To Employee Wage Computation Program");
 
         int totalMonthWage = 0;
         int workingDay = 0;
-        while (workingDay < WORKING_DAY_IN_MONTH) {
+        int workingHour = 0;
+        while (workingDay < WORKING_DAY_IN_MONTH || workingHour < TOTAL_WORKING_HOUR) {
             int employeeType = isPresent();
             switch (employeeType) {
                 case 1:
                     System.out.println("Employee is FullTime DailyWage: " + calculateWage(FULL_TIME));
                     totalMonthWage += calculateWage(FULL_TIME);
                     workingDay++;
+                    workingHour += FULL_TIME;
                     break;
                 case 2:
                     System.out.println("Employee is PartTime DailyWage: " + calculateWage(PART_TIME));
                     totalMonthWage += calculateWage(PART_TIME);
+                    workingHour += PART_TIME;
                     workingDay++;
                     break;
                 case 3:
@@ -31,8 +35,15 @@ public class App {
                     System.out.println("This Person Is not An Employee");
                     break;
             }
+            if (workingHour >= TOTAL_WORKING_HOUR) {
+                workingHour = TOTAL_WORKING_HOUR;
+                break;
+            }
+
         }
         System.out.println("Total Wage For Month : " + totalMonthWage);
+        System.out.println("Total Time : " + workingHour);
+        System.out.println("Total Days : " + workingDay);
     }
 
     private static int calculateWage(int time) {
